9fa4bab2dc734e384572138d2601d523d8bb2b3a
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-13 18:21:16 +0500

    apdated svg attr

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -134,8 +134,7 @@ const ShoppingCart = () => {
                           className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50"
                           onClick={() => handleIncrement(product?.id)}
                         >
-                          {" "}
-                          +{" "}
+                          +
                         </span>
                       </div>
                       <div className="flex items-center space-x-4">
@@ -152,14 +151,14 @@ const ShoppingCart = () => {
                           xmlns="http://www.w3.org/2000/svg"
                           fill="none"
                           viewBox="0 0 24 24"
-                          stroke-width="1.5"
+                          strokeWidth="1.5"
                           stroke="currentColor"
                           className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
                           onClick={() => removeCart(product.id)}
                         >
                           <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                             d="M6 18L18 6M6 6l12 12"
                           />
                         </svg>
